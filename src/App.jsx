c69ea1352157cf4cb907cd960714d816700682de
Andrea McKenzie <mckenzie.andrea.m@gmail.com>
2019-05-08 12:59:22 -0400

    started integrating codemirror

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@
 // import logo from './logo.svg';
 // import './App.css';
 
-
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 import { HashLink as Link } from 'react-router-hash-link'
+import  CodeMirror from 'react-codemirror';
 
 
 import Nav from './components/Nav'
@@ -13,19 +13,27 @@ import TextEditingContainer from './components/TextEditingContainer'
 import PCInputContainer from './components/PCInputContainer'
 import Title from './components/Title'
 
-// import Footer from './components/footer'
-// import ContactMe from './components/contactme'
-// import ProjectContainer from './components/project-container'
-
 import './reset.css';
 import './App.css';
+import 'codemirror/lib/codemirror.css';
+
+import 'codemirror/mode/javascript/javascript.js'
 
 function App() {
+
+	let options = {
+		lineNumbers: true,
+		showCursorWhenSelecting: true,
+		mode: 'javascript'
+
+	};
+
   return (
     <div className="App">
     	<Title />
     	<TextEditingContainer />
       	<PCInputContainer />
+      	<CodeMirror value="console.log('Hello, World!')" options={options}/>
     </div>
   );
 }
