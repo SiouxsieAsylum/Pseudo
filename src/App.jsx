28592325553f192da3d12bbe0d65c297a20d9819
Andrea McKenzie <mckenzie.andrea.m@gmail.com>
2019-05-08 14:10:36 -0400

    added theme for codemirror

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,43 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+/////////////////////// IMPORTS /////////////////////////////////
+/////////////////// IMPORTS - NPM ///////////////////////////////
 
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 import { HashLink as Link } from 'react-router-hash-link'
 import  CodeMirror from 'react-codemirror';
 
+/////////////// IMPORTS - COMPONENTS ////////////////////////////
 
 import Nav from './components/Nav'
 import TextEditingContainer from './components/TextEditingContainer'
 import PCInputContainer from './components/PCInputContainer'
 import Title from './components/Title'
 
+
+/////////////////// IMPORTS - CSS //////////////////////////////
+
 import './reset.css';
 import './App.css';
 import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/midnight.css'
+
+
+//////////////////// IMPORTS - JS //////////////////////////////
 
 import 'codemirror/mode/javascript/javascript.js'
+
+
+/////////////////// COMPONENT - APP /////////////////////////
 
 function App() {
 
 	let options = {
 		lineNumbers: true,
 		showCursorWhenSelecting: true,
-		mode: 'javascript'
+		//eventually a state
+		mode: 'javascript',
+		//eventually a state
+		theme: 'midnight'
 
 	};
 
@@ -33,7 +46,10 @@ function App() {
     	<Title />
     	<TextEditingContainer />
       	<PCInputContainer />
-      	<CodeMirror value="console.log('Hello, World!')" options={options}/>
+      	 {/* height is 300 default, width is up to container */}
+      	<div className="code-mirror-sizer">
+      		<CodeMirror value="console.log('Hello, World!')" options={options}/>
+      	</div>
     </div>
   );
 }
